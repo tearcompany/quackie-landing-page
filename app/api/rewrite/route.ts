3ad3a836b5c167cfbd59bf17bd79912ee77d6a56
Michał Porydzaj <michal.porydzaj@portalyogi.pl>
2026-07-04 09:29:16 +0200

    🐄 update: nudge the prompt from v15 to v16

--- app/api/rewrite/route.ts
+++ app/api/rewrite/route.ts
@@ -4,7 +4,7 @@ import OpenAI from 'openai'
 export const runtime = 'nodejs'
 
 const DEFAULT_PROMPT_ID = 'pmpt_6a47dda639808194877bc3e2d961224307ddfd2442c27bfb'
-const DEFAULT_PROMPT_VERSION = '15'
+const DEFAULT_PROMPT_VERSION = '16'
 const MAX_TEXT_LENGTH = 2000
 
 interface RewriteRequestBody {
